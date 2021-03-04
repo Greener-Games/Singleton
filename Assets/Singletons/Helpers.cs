19208f47adb4299e9e2751ef38768f376aed22b3
Tom Greener <thomas.greener@hotmail.co.uk>
2021-03-04 10:24:25 +0000

    mess up with merge, refixed to be correct
    Update Generate Documentation.yml
    
    Update Generate Documentation.yml
    
    Update Generate Documentation.yml
    
    Update Generate Documentation.yml
    
    Update Generate Documentation.yml
    
    Update Generate Documentation.yml
    
    Update Generate Documentation.yml
    
    Update Generate Documentation.yml
    
    Update Generate Documentation.yml
    
    Update Generate Documentation.yml
    
    Update Generate Documentation.yml
    
    Update Generate Documentation.yml

--- Assets/Singletons/Helpers.cs
+++ Assets/Singletons/Helpers.cs
@@ -1,48 +1,62 @@
-﻿using System;
-using System.Collections;
+﻿#region
+
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+#endregion
+
 namespace GG.Singletons
 {
-    internal class Helpers
+    static class Helpers
     {
-        internal static List<GameObject> dontDestroyOnLoads = new List<GameObject>();
-
+        static readonly List<GameObject> DontDestroyOnLoads = new List<GameObject>();
+        
         /// <summary>
-        /// Workaround to help find objects marked as dont destroy on load
+        ///     Workaround to help find objects marked as dont destroy on load
         /// </summary>
         /// <param name="go"></param>
         internal static void RegisterDontDestroy(GameObject go)
         {
-            dontDestroyOnLoads.Add(go);
+            DontDestroyOnLoads.Add(go);
+        }
+        
+        /// <summary>
+        /// remove a game object from the global cache
+        /// </summary>
+        /// <param name="go"></param>
+        public static void DeRegisterDontDestroy(GameObject go)
+        {
+            if (DontDestroyOnLoads.Contains(go))
+            {
+                DontDestroyOnLoads.Remove(go);
+            }
         }
 
-/// <summary>
-/// Finds any object of type regardless if it is enabled or not
-/// </summary>
-/// <typeparam name="T"></typeparam>
-/// <returns></returns>
+        /// <summary>
+        ///     Finds any object of type regardless if it is enabled or not
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         internal static List<T> FindObjectsOfTypeAll<T>()
         {
-
             List<T> results = new List<T>();
             for (int i = 0; i < SceneManager.sceneCount; i++)
             {
-                var s = SceneManager.GetSceneAt(i);
+                Scene s = SceneManager.GetSceneAt(i);
                 if (s.isLoaded)
                 {
-                    var allGameObjects = s.GetRootGameObjects();
+                    GameObject[] allGameObjects = s.GetRootGameObjects();
                     for (int j = 0; j < allGameObjects.Length; j++)
                     {
-                        var go = allGameObjects[j];
+                        GameObject go = allGameObjects[j];
                         results.AddRange(go.GetComponentsInChildren<T>(true));
                     }
                 }
             }
 
-            foreach (GameObject savedObject in dontDestroyOnLoads)
+            //if we are quitting then these objects may be destroyed so dont check
+            foreach (GameObject savedObject in DontDestroyOnLoads)
             {
                 results.AddRange(savedObject.GetComponentsInChildren<T>(true));
             }
